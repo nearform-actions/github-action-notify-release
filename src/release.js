a5ca3c084acf0bf0f1c5b787621548dec0c02341
gilach <grzegorz@ilach.pl>
2021-05-11 10:22:12 +0200

    refactor: variable rename

--- src/release.js
+++ src/release.js
@@ -5,12 +5,12 @@ async function getLatestRelease(token) {
   const octokit = github.getOctokit(token);
   const { owner, repo } = github.context.repo;
 
-  const allReleasesResp = await octokit.request(`GET /repos/{owner}/{repo}/releases/latest`, {
+  const latestReleaseResponse = await octokit.request(`GET /repos/{owner}/{repo}/releases/latest`, {
     owner,
     repo,
   });
 
-  return allReleasesResp.data;
+  return latestReleaseResponse.data;
 }
 
 async function getUnreleasedCommits(token, latestReleaseDate, staleDays) {
