86936ce25fcaae98959a4a4780398d64ba5a744e
Sameer Srivastava <sameer.srivastava@nearform.com>
2022-01-27 20:17:30 +0530

    chore: add action ref warning

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ const core = require('@actions/core')
 
 const { runAction } = require('./release-notify-action')
 
+const actionRef = process.env.GITHUB_ACTION_REF
+
 async function run() {
+  if (actionRef === 'main' || actionRef === 'master') {
+    core.warning(
+      `nearform/github-action-notify-release is pinned at HEAD. We strongly ` +
+        `advise against pinning to "@master" as it may be unstable. Please ` +
+        `update your GitHub Action YAML from:\n\n` +
+        `    uses: 'nearform/github-action-notify-release@master'\n\n` +
+        `to:\n\n` +
+        `    uses: 'nearform/github-action-notify-release@v0'\n\n` +
+        `Alternatively, you can pin to any git tag or git SHA in the ` +
+        `repository.`,
+    )
+  }
   const token = core.getInput('github-token', { required: true })
   const staleDays = Number(core.getInput('stale-days'))
   const commitMessageLines = Number(core.getInput('commit-messages-lines'))
