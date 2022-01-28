1d74ad735dae7c5dfc22ef36b48ed333f847d7ff
Sameer Srivastava <sameer.srivastava@nearform.com>
2022-01-28 16:07:16 +0530

    chore: get latest major tag for warning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict'
 const core = require('@actions/core')
+const exec = require('@actions/exec');
 
 const { runAction } = require('./release-notify-action')
 
@@ -7,13 +8,14 @@ const actionRef = process.env.GITHUB_ACTION_REF
 
 async function run() {
   if (actionRef === 'main' || actionRef === 'master') {
+    const latestMajorTag = await getLatestMajorTag()
     core.warning(
       `nearform/github-action-notify-release is pinned at HEAD. We strongly ` +
         `advise against pinning to "@master" as it may be unstable. Please ` +
         `update your GitHub Action YAML from:\n\n` +
         `    uses: 'nearform/github-action-notify-release@master'\n\n` +
         `to:\n\n` +
-        `    uses: 'nearform/github-action-notify-release@v0'\n\n` +
+        `    uses: 'nearform/github-action-notify-release@${latestMajorTag}'\n\n` +
         `Alternatively, you can pin to any git tag or git SHA in the ` +
         `repository.`
     )
@@ -23,6 +25,25 @@ async function run() {
   const commitMessageLines = Number(core.getInput('commit-messages-lines'))
 
   await runAction(token, staleDays, commitMessageLines)
+}
+
+async function getLatestMajorTag() {
+  let latestTag = ''
+  let error = ''
+
+  const options = {}
+  options.listeners = {
+    stdout: (data) => {
+      latestTag += data.toString()
+    },
+    stderr: (data) => {
+      error += data.toString()
+    }
+  }
+  await exec.exec('git', ['describe', '--abbrev=0', '--tags'], options)
+  const latestMajorTag = latestTag.split('.')[0] || ''
+
+  return error ? '' : latestMajorTag
 }
 
 run().catch((err) => core.setFailed(err))
