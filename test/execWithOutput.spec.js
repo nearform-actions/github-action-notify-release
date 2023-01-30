8dbb97378d17328291e47be5dd2a3c012fcae40e
Peter Jones <hi@peterjones.io>
2023-01-30 10:01:04 +0000

    half tested execWithOutput

--- test/execWithOutput.spec.js
+++ test/execWithOutput.spec.js
@@ -1,73 +1,61 @@
 'use strict'
 
-const tap = require('tap')
-const sinon = require('sinon')
-const proxyquire = require('proxyquire')
+const { exec } = require('@actions/exec')
 
-const execStub = sinon.stub()
+const execWithOutputModule = require('../src/utils/execWithOutput')
 
-const execWithOutputModule = proxyquire('../src/utils/execWithOutput', {
-  '@actions/exec': {
-    exec: execStub,
-  },
-})
+jest.mock('@actions/exec', () => ({
+  exec: jest.fn(),
+}))
 
-tap.afterEach(() => {
-  sinon.restore()
-})
+test('resolves with output of the exec command if exit code is 0', async () => {
+  const output = 'output'
 
-tap.test(
-  'resolves with output of the exec command if exit code is 0',
-  async (t) => {
-    const output = 'output'
+  exec.mockImplementation((_, __, options) => {
+    options.listeners.stdout(Buffer.from('output', 'utf8'))
 
-    execStub.callsFake((_, __, options) => {
-      options.listeners.stdout(Buffer.from(output, 'utf8'))
-
-      return Promise.resolve(0)
-    })
-
-    t.resolves(execWithOutputModule.execWithOutput('ls', ['-al']), output)
-    execStub.calledWith('ls', ['-al'])
-  }
-)
-
-tap.test(
-  'Throws with output of the exec command if exit code is not 0',
-  async (t) => {
-    const output = 'output'
-
-    execStub.callsFake((_, __, options) => {
-      options.listeners.stderr(Buffer.from(output, 'utf8'))
-      return Promise.reject(new Error())
-    })
-
-    t.rejects(
-      () => execWithOutputModule.execWithOutput('ls', ['-al']),
-      'Error: ls -al returned code 1  \nSTDOUT:  \nSTDERR: ${output}'
-    )
-
-    execStub.calledWith('ls', ['-al'])
-  }
-)
-
-tap.test('provides cwd to exec function', async () => {
-  const cwd = './'
-
-  execStub.resolves(0)
-  execWithOutputModule.execWithOutput('command', [], cwd)
-  execStub.calledWith('command', [], { cwd })
-})
-
-tap.test('rejects if exit code is not 0', async (t) => {
-  const errorOutput = 'error output'
-
-  execStub.callsFake((_, __, options) => {
-    options.listeners.stderr(Buffer.from(errorOutput, 'utf8'))
-
-    return Promise.resolve(1)
+    return Promise.resolve(0)
   })
 
-  t.rejects(execWithOutputModule.execWithOutput('command'))
-  execStub.calledWith('command')
+  await expect(
+    execWithOutputModule.execWithOutput('ls', ['-al'])
+  ).resolves.toBe(output)
+
+  expect(exec).toHaveBeenCalled()
 })
+
+test('Throws with output of the exec command if exit code is not 0', async () => {
+  const output = 'output'
+
+  exec.mockImplementation((_, __, options) => {
+    options.listeners.stderr(Buffer.from(output, 'utf8'))
+    return Promise.reject(new Error())
+  })
+
+  await expect(
+    execWithOutputModule.execWithOutput('ls', ['-al'])
+  ).rejects.toThrow(`ls -al returned code 1 \nSTDOUT: \nSTDERR: ${output}`)
+
+  expect(exec).toHaveBeenCalled()
+})
+
+// tap.test('provides cwd to exec function', async () => {
+//   const cwd = './'
+
+//   execStub.resolves(0)
+//   execWithOutputModule.execWithOutput('command', [], cwd)
+//   execStub.calledWith('command', [], { cwd })
+// })
+
+// tap.test('rejects if exit code is not 0', async (t) => {
+//   const errorOutput = 'error output'
+
+//   execStub.callsFake((_, __, options) => {
+//     options.listeners.stderr(Buffer.from(errorOutput, 'utf8'))
+
+//     return Promise.resolve(1)
+//   })
+
+//   t.rejects(execWithOutputModule.execWithOutput('command'))
+//   execStub.calledWith('command')
+// })
