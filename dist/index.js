8f2bd8ffe9a84822df5e5b35b69056313e668b0d
Peter Jones <hi@peterjones.io>
2023-02-03 11:23:47 +0000

    replace remaining instances of core.getInput with inputs

--- dist/index.js
+++ dist/index.js
@@ -70402,7 +70402,7 @@ async function run({ inputs }) {
     toolkit.logActionRefWarning()
     toolkit.logRepoWarning()
 
-    const token = core.getInput('github-token', { required: true })
+    const token = inputs('github-token', { required: true })
 
     const notifyAfter = parseNotifyAfter(
       inputs['notify-after'],
@@ -70424,7 +70424,7 @@ async function run({ inputs }) {
     }
 
     logInfo('Workflow dispatched or release published ...')
-    const commitMessageLines = Number(core.getInput('commit-messages-lines'))
+    const commitMessageLines = Number(inputs('commit-messages-lines'))
     await runAction(token, notifyAfter, commitMessageLines)
   } catch (err) {
     core.setFailed(err)
